54fca7f00cfaf636413f69bf660ba5238122674b
delivan <wjdgurdl272@gmail.com>
2023-08-13 19:11:02 +0900

    Add zora network to config & `chainSymbolImageUrl` for each chain

--- packages/extension/src/config.ts
+++ packages/extension/src/config.ts
@@ -4,53 +4,6 @@ import { ChainInfo } from "@keplr-wallet/types";
 export const EmbedChainInfos: ChainInfo[] = [
   {
     evm: {
-      chainId: "420",
-      chainName: "Optimism Goerli",
-      jsonRpcProvider: "https://goerli.optimism.io",
-      nativeCurrency: {
-        coinDenom: "ETH",
-        coinMinimalDenom: "wei",
-        coinDecimals: 18,
-        coinImageUrl:
-          "https://avatars.githubusercontent.com/u/6250754?s=200&v=4",
-      },
-    },
-    rpc: "https://goerli.optimism.io",
-    rest: "https://goerli.optimism.io",
-    chainId: "420",
-    chainName: "Optimism Goerli",
-    stakeCurrency: {
-      coinDenom: "ETH",
-      coinMinimalDenom: "wei",
-      coinDecimals: 18,
-      coinImageUrl: "https://avatars.githubusercontent.com/u/6250754?s=200&v=4",
-    },
-    bip44: {
-      coinType: 60,
-    },
-    bech32Config: Bech32Address.defaultBech32Config("dummy"),
-    currencies: [
-      {
-        coinDenom: "ETH",
-        coinMinimalDenom: "wei",
-        coinDecimals: 18,
-        coinImageUrl:
-          "https://avatars.githubusercontent.com/u/6250754?s=200&v=4",
-      },
-    ],
-    feeCurrencies: [
-      {
-        coinDenom: "ETH",
-        coinMinimalDenom: "wei",
-        coinDecimals: 18,
-        coinImageUrl:
-          "https://avatars.githubusercontent.com/u/6250754?s=200&v=4",
-      },
-    ],
-    features: ["eth-address-gen", "eth-key-sign"],
-  },
-  {
-    evm: {
       chainId: "10",
       chainName: "Optimism",
       jsonRpcProvider: "https://mainnet.optimism.io",
@@ -67,6 +20,8 @@ export const EmbedChainInfos: ChainInfo[] = [
     rest: "https://mainnet.optimism.io",
     chainId: "10",
     chainName: "Optimism",
+    chainSymbolImageUrl:
+      "https://avatars.githubusercontent.com/u/58791460?s=200&v=4",
     stakeCurrency: {
       coinDenom: "ETH",
       coinMinimalDenom: "wei",
@@ -118,6 +73,62 @@ export const EmbedChainInfos: ChainInfo[] = [
     rest: "https://mainnet.base.org",
     chainId: "8453",
     chainName: "Base",
+    chainSymbolImageUrl:
+      "https://raw.githubusercontent.com/base-org/brand-kit/main/logo/in-product/Base_Network_Logo.svg",
+    stakeCurrency: {
+      coinDenom: "ETH",
+      coinMinimalDenom: "wei",
+      coinDecimals: 18,
+      coinImageUrl:
+        "https://ethereum.org/static/6b935ac0e6194247347855dc3d328e83/6ed5f/eth-diamond-black.webp",
+      coinGeckoId: "ethereum",
+    },
+    bip44: {
+      coinType: 60,
+    },
+    bech32Config: Bech32Address.defaultBech32Config("dummy"),
+    currencies: [
+      {
+        coinDenom: "ETH",
+        coinMinimalDenom: "wei",
+        coinDecimals: 18,
+        coinImageUrl:
+          "https://ethereum.org/static/6b935ac0e6194247347855dc3d328e83/6ed5f/eth-diamond-black.webp",
+        coinGeckoId: "ethereum",
+      },
+    ],
+    feeCurrencies: [
+      {
+        coinDenom: "ETH",
+        coinMinimalDenom: "wei",
+        coinDecimals: 18,
+        coinImageUrl:
+          "https://avatars.githubusercontent.com/u/6250754?s=200&v=4",
+        coinGeckoId: "ethereum",
+      },
+    ],
+    features: ["eth-address-gen", "eth-key-sign"],
+  },
+  {
+    evm: {
+      chainId: "7777777",
+      chainName: "Zora",
+      jsonRpcProvider: "https://rpc.zora.energy",
+      nativeCurrency: {
+        coinDenom: "ETH",
+        coinMinimalDenom: "wei",
+        coinDecimals: 18,
+        coinImageUrl:
+          "https://avatars.githubusercontent.com/u/6250754?s=200&v=4",
+        coinGeckoId: "ethereum",
+      },
+    },
+    rpc: "https://rpc.zora.energy",
+    rest: "https://rpc.zora.energy",
+    chainId: "7777777",
+    chainName: "Zora",
+    chainSymbolImageUrl:
+      "https://raw.githubusercontent.com/ourzora/zora-docs/main/static/img/zoraOrb.svg",
     stakeCurrency: {
       coinDenom: "ETH",
       coinMinimalDenom: "wei",
